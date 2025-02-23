62dde92bc76e4800ee3f73517b98227942415fba
Ivan Sigaev <ivan.develop@gmail.com>
2025-02-23 20:48:39 +0100

    feat(task2): add lambda functions

---
+++ src/products-mock.ts
@@ -0,0 +1,9 @@
+import { Product } from "./types/product";
+
+export const products: Product[] = [
+  { id: '1', title: 'Product 1', description: 'Description 1', price: 100 },
+  { id: '2', title: 'Product 2', description: 'Description 2', price: 200 },
+  { id: '3', title: 'Product 3', description: 'Description 3', price: 300 },
+  { id: '4', title: 'Product 4', description: 'Description 4', price: 400 },
+  { id: '5', title: 'Product 5', description: 'Description 5', price: 500 },
+];
