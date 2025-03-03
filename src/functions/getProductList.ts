4ab87dbb011e36c6db7be531b83db9da53716ac1
Ivan Sigaev <ivan.develop@gmail.com>
2025-03-03 07:34:43 +0100

    feat(task4): adjust getProductList

--- src/functions/getProductList.ts
+++ src/functions/getProductList.ts
@@ -1,18 +1,37 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { products } from "../products-mock";
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { Product } from "../types/product";
+import { Stock } from "../types/stock";
+
+const client = new DynamoDBClient();
+const dynamodb = DynamoDBDocumentClient.from(client);
+const productTable = process.env.PRODUCTS_TABLE || 'products';
+const stockTable = process.env.STOCKS_TABLE || 'stocks';
 
 export const getProductList = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
+    const productsResult = await dynamodb.send(new ScanCommand({ TableName: productTable, }));
+    const products: Product[] = productsResult.Items as Product[] || [];
+    const stocksResult = await dynamodb.send(new ScanCommand({ TableName: stockTable, }));
+    const stocks: Stock[] = stocksResult.Items as Stock[] || [];
+
+    const response = products.map(product => {
+      return {
+        ...product,
+        ...stocks.find(s => s.product_id === product.id) || { count: 0 }
+      }
+    });
+
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
       },
-      body: JSON.stringify(products),
+      body: JSON.stringify(response),
     };
   } catch (error) {
-    console.error('Error:', error);
     return {
       statusCode: 500,
       headers: {
