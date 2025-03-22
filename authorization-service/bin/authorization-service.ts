2019aa5980730322145c3f6b2e58ae1e71a4a89e
Ivan Sigaev <ivan.develop@gmail.com>
2025-03-22 20:33:07 +0100

    Revert "feat(task7): add auth service"
    This reverts commit cd27f2634914efb84712ec5ba56fefffcc628089.

--- authorization-service/bin/authorization-service.ts
+++
@@ -1,7 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { AuthorizationServiceStack } from '../lib/authorization-service-stack';
-
-const app = new cdk.App();
-new AuthorizationServiceStack(app, 'AuthorizationServiceStack');
-app.synth();
-
